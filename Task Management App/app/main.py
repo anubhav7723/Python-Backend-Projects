a250e33143d7e21eaa6f58163ff1c72aaf5296eb
Anubhav Gupta <anubhavg0098@gmail.com>
2026-07-05 00:22:23 +0530

    GET POST PUT DELETE api added

--- Task Management App/app/main.py
+++ Task Management App/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from app.schemas.task import TaskCreate
+from fastapi import HTTPException
+
 
 app = FastAPI()
 
-print("Server Started")
+temp_memo = []
+task_id = 1
 
 @app.get("/")
 def root():
@@ -12,7 +15,56 @@ def root():
 
 @app.post("/tasks")
 def create_task(task: TaskCreate):
-    return {
-        "message": "Task Created successfully",
-        "task" : task
+    global task_id
+    
+    new_task = {
+        "id" : task_id,
+        "title" : task.title,
+        "description": task.description,
+        "priority": task.priority
     }
+    
+    temp_memo.append(new_task)
+    task_id += 1
+     
+    return new_task
+    
+@app.get("/tasks")
+def get_task():
+    return temp_memo
+
+@app.get("/tasks/{call_id}")
+def get_task_with_id(call_id : int):
+    for task in temp_memo:
+        if task["id"] == call_id:
+            return task
+        
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
+        
+@app.put("/tasks/{call_id}")
+def update_task(call_id : int, new_message :TaskCreate):
+    for task in temp_memo:
+        if task["id"] == call_id:
+            task.update(new_message.model_dump())
+            return task
+        
+    raise HTTPException(
+        status_code=404, 
+        detail="Task Not Found"
+    )
+    
+@app.delete("/tasks/{call_id}")
+def remove_task(call_id : int):
+    for task in temp_memo:
+        if task["id"] == call_id:
+            temp_memo.remove(task)
+            
+            return {"message": "Successfully Removed"}
+        
+    raise HTTPException(
+        status_code=404, 
+        detail="Task not found"
+    )
