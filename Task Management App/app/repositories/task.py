33ccddb024cd8098a255ba31a2718b58c98e8f4b
Anubhav Gupta <anubhavg0098@gmail.com>
2026-07-16 15:29:19 +0530

    4 method implemented

--- Task Management App/app/repositories/task.py
+++ Task Management App/app/repositories/task.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 from app.models.task import Task
 from app.schemas.task import TaskCreate
@@ -8,7 +9,6 @@ class TaskRepository:
 
     @staticmethod
     def create(db: Session, task: TaskCreate):
-
         db_task = Task(
             title=task.title,
             description=task.description,
@@ -20,3 +20,45 @@ class TaskRepository:
         db.refresh(db_task)
 
         return db_task
+    
+    @staticmethod
+    def get_all(db: Session):
+        toprint = select(Task)
+        res = db.execute(toprint)
+        return res.scalars().all()
+    
+    @staticmethod
+    def get_by_id(db: Session, task_id: int):
+        toprint = select(Task).where(Task.id == task_id)
+        
+        res = db.execute(toprint)
+        
+        return res.scalar_one_or_none()
+    
+    @staticmethod
+    def update(db: Session, task_id: int, task_data: TaskCreate):
+        task = TaskRepository.get_by_id(db, task_id)
+        
+        if task is None:
+            return None
+        
+        task.title = task_data.title
+        task.description = task_data.description
+        task.priority = task_data.priority
+        
+        db.commit()
+        db.refresh(task)
+        
+        return task
+    
+    @staticmethod
+    def delete(db: Session, task_id:int):
+        task = TaskRepository.get_by_id(db, task_id)
+        
+        if task is None:
+            return None
+        
+        db.delete(task)
+        db.commit()
+        
+        return {"message": f"Task with id {task_id} deleted successfully"}
