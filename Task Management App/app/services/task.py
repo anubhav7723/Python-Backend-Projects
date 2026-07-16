d31207f82a3e737764b165e47485bbc29d5ae159
Anubhav Gupta <anubhavg0098@gmail.com>
2026-07-16 15:30:29 +0530

    4 method implemented

--- Task Management App/app/services/task.py
+++ Task Management App/app/services/task.py
@@ -8,5 +8,21 @@ class TaskService:
 
     @staticmethod
     def create_task(db: Session, task: TaskCreate):
-
         return TaskRepository.create(db, task)
+    
+    @staticmethod
+    def get_all_tasks(db: Session):
+        return TaskRepository.get_all(db)
+    
+    @staticmethod
+    def get_task_by_id(db:Session, task_id: int):
+        return TaskRepository.get_by_id(db, task_id)
+    
+    @staticmethod
+    def update_task(db: Session, task_id: int, task_data: TaskCreate):
+        return TaskRepository.update(db, task_id, task_data)
+    
+    @staticmethod
+    def delete_task(db: Session, task_id: int):
+        return TaskRepository.delete(db, task_id)
+    
