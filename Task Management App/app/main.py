612634785361d09bea8a3c69c2e6c86c43674852
Anubhav Gupta <anubhavg0098@gmail.com>
2026-07-16 18:18:42 +0530

    Main architecture

--- Task Management App/app/main.py
+++ Task Management App/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from app.routers.task import router as task_router
+from app.routers.auth import router as auth_router
 
 app = FastAPI()
 
 app.include_router(task_router)
+app.include_router(auth_router)
