0f8e610c922819ace92ea24cce9ef45b8d04bc61
Anubhav Gupta <anubhavg0098@gmail.com>
2026-07-16 18:52:49 +0530

    Login Flow with JWT

--- Task Management App/app/routers/auth.py
+++ Task Management App/app/routers/auth.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from app.dependencies import get_db
 
 from app.schemas.user import UserCreate
+from app.schemas.user import UserLogin
 from app.services.auth import AuthService
 
 router = APIRouter(
@@ -12,9 +13,23 @@ router = APIRouter(
     tags=["Auth"]
 )
 
+@router.get("/")
+def get_all_users(
+    db: Session = Depends(get_db)
+):
+    return AuthService.get_all_users(db)
+
+
 @router.post("/register")
 def create_user(
     user: UserCreate,
     db: Session = Depends(get_db)
 ):
     return AuthService.create_user(db, user)
+
+@router.post("/login")
+def login(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+    return AuthService.login(db, user)
--- Task Management App/app/services/auth.py
+++ Task Management App/app/services/auth.py
@@ -3,10 +3,15 @@ from sqlalchemy.orm import Session
 
 from app.schemas.user import UserCreate
 from app.repositories.user import UserRepository
+from app.schemas.user import UserLogin
 
-from app.core.security import hash_password
+from app.core.security import hash_password, verify_password, create_access_token
 
 class AuthService:
+    @staticmethod
+    def get_all_users(db: Session):
+        return UserRepository.get_all_users(db)
+    
     @staticmethod
     def create_user(db: Session, user: UserCreate):
         existing_user = UserRepository.get_by_email(db, user.email)
@@ -20,3 +25,28 @@ class AuthService:
         hashed_password = hash_password(user.password)
         
         return UserRepository.create(db, user.username, user.email, hashed_password)
+    
+    @staticmethod
+    def login(db: Session, user: UserLogin):
+        db_user = UserRepository.get_by_email(db, user.email)
+        
+        if db_user is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid email or Password"
+            )
+        
+        if not verify_password(user.password, db_user.hashed_password):
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid Email or Password"
+            )
+            
+        token = create_access_token({
+            "sub": db_user.email
+        })
+        
+        return {
+            "access token" : token,
+            "token_type": "bearer"
+        }
